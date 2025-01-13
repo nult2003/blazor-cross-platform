4740ea1b89faba0359c80db8b292ea608d1f2043
NhatLe <nult2003@gmail.com>
2025-01-13 23:16:09 +0700

    Add integrate blazor with web MVC

---
+++ New42WPF/AppState.cs
@@ -0,0 +1,9 @@
+﻿using New42WPF;
+
+namespace WpfBlazor;
+
+public static class AppState
+{
+    public static New42WPF.Index IndexComponent { get; set; }
+    public static MainWindow MainWindow { get; set; }
+}
---
+++ WebMVCVsBlazor/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace WebMVCVsBlazor.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
